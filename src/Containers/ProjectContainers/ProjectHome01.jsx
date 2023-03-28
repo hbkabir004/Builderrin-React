c1b92e5a09e99f076161a5a6137fc2bbf8dcbe67
ABIR <abircse004@gmail.com>
2023-03-28 14:22:11 +0600

    Home Projects Pages Added

--- src/Containers/ProjectContainers/ProjectHome01.jsx
+++ src/Containers/ProjectContainers/ProjectHome01.jsx
@@ -1,7 +1,77 @@
-import React from 'react';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import Isotope from 'isotope-layout';
+import React, { useEffect, useState } from 'react';
+import ProjectCard02 from '../../Components/ProjectElements/ProjectCard02';
 
 const ProjectHome01 = () => {
+    const ProjectHomeData01 = [
+        {
+            id: '1',
+            img: 'images/team/Project_2_Images/3.png',
+            tag: 'residential business',
+            title: 'Large Garden Lodge',
+            city: 'NEW YORK',
+        },
+        {
+            id: '2',
+            img: 'images/team/Project_2_Images/4.png',
+            tag: 'highrise',
+            title: 'Commercial Building',
+            city: 'NEW YORK',
+        },
+        {
+            id: '3',
+            img: 'images/team/Project_2_Images/5.png',
+            tag: 'residential commercial',
+            title: 'Modern Pent House',
+            city: 'NEW YORK',
+        },
+        {
+            id: '4',
+            img: 'images/team/Project_2_Images/1.png',
+            tag: 'highrise commercial',
+            title: 'Large Swiming Pool',
+            city: 'NEW YORK',
+        },
+        {
+            id: '5',
+            img: 'images/team/Project_2_Images/2.png',
+            tag: 'business highrise',
+            title: 'Highrise Architecture',
+            city: 'NEW YORK',
+        },
+
+    ];
+
+    const [filterKey, setFilterKey] = useState('*')
+
+    useEffect(() => {
+        Isotope.current = new Isotope('.grid', {
+            itemSelector: '.element-item',
+            percentPosition: true,
+            // layoutMode: 'packery',
+            // masonry: {
+            //     columnWidth: '.grid-sizer'
+            // },
+            transformsEnabled: true,
+            transitionDuration: "700ms",
+            resize: true,
+            fitWidth: true,
+            // columnWidth: '.grid-sizer',
+        })
+
+        // cleanup
+        return () => Isotope.current.destroy()
+    }, []);
+
+    // handling filter key change
+    useEffect(() => {
+        filterKey === '*'
+            ? Isotope.current.arrange({ filter: `*` })
+            : Isotope.current.arrange({ filter: `.${filterKey}` })
+    }, [filterKey]);
+
+    const handleFilterKeyChange = key => () => setFilterKey(key);
+
     return (
         <div className="section project_iso project_iso1">
             <div className="container-fluid g-0">
@@ -12,7 +82,7 @@ const ProjectHome01 = () => {
                 </div>
                 <div className="row g-0">
                     <div className="col">
-                        <Tabs>
+                        {/* <Tabs>
                             <TabList className="button-group filters-button-group">
                                 <Tab className="button is-checked" data-filter="*">All</Tab>
                                 <Tab className="button" data-filter=".commercial">Commercial</Tab>
@@ -147,7 +217,26 @@ const ProjectHome01 = () => {
                             </div>
 
 
-                        </Tabs>
+                        </Tabs> */}
+
+                        <div className="button-group filters-button-group">
+                            <button className="button" onClick={handleFilterKeyChange('*')}>All <sup className="filter-count"></sup></button>
+                            <button className="button" onClick={handleFilterKeyChange('commercial')}>Commercial <sup className="filter-count"></sup></button>
+                            <button className="button" onClick={handleFilterKeyChange('highrise')}>Highrise <sup className="filter-count"></sup></button>
+                            <button className="button" onClick={handleFilterKeyChange('residential')}>Residential <sup className="filter-count"></sup></button>
+                            <button className="button" onClick={handleFilterKeyChange('business')}>Business <sup className="filter-count"></sup></button>
+
+                        </div>
+
+                        <div class="grid grid-5">
+                            {
+                                ProjectHomeData01.map(data => <ProjectCard02
+                                    key={data.id}
+                                    data={data}
+                                />)
+                            }
+
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Containers/ProjectContainers/ProjectHome02.jsx
+++ src/Containers/ProjectContainers/ProjectHome02.jsx
@@ -1,7 +1,125 @@
-import React from 'react';
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import Isotope from 'isotope-layout';
+import React, { useEffect, useState } from 'react';
+import ProjectCard01 from '../../Components/ProjectElements/ProjectCard01';
 
 const ProjectHome02 = () => {
+    const ProjectHomeData02 = [
+        {
+            id: '1',
+            img: 'images/team/1.png',
+            tag: 'residential business',
+            title: 'Large Garden Lodge',
+            city: 'NEW YORK',
+        },
+        {
+            id: '2',
+            img: 'images/team/2.png',
+            tag: 'highrise',
+            title: 'Commercial Building',
+            city: 'NEW YORK',
+        },
+        {
+            id: '3',
+            img: 'images/team/3.png',
+            tag: 'residential commercial',
+            title: 'Modern Pent House',
+            city: 'NEW YORK',
+        },
+        {
+            id: '4',
+            img: 'images/team/5.png',
+            tag: 'highrise commercial',
+            title: 'Large Swiming Pool',
+            city: 'NEW YORK',
+        },
+        {
+            id: '5',
+            img: 'images/team/9.png',
+            tag: 'business highrise',
+            title: 'Highrise Architecture',
+            city: 'NEW YORK',
+        },
+        {
+            id: '6',
+            img: 'images/team/10.png',
+            tag: 'residential',
+            title: 'Curve Sky Tower',
+            city: 'NEW YORK',
+        },
+        {
+            id: '7',
+            img: 'images/team/11.png',
+            tag: 'residential',
+            title: 'Shopping Mall',
+            city: 'NEW YORK',
+        },
+        {
+            id: '8',
+            img: 'images/team/12.png',
+            tag: 'business residential',
+            title: 'Wonderful Bridge',
+            city: 'NEW YORK',
+        },
+        {
+            id: '9',
+            img: 'images/team/13.png',
+            tag: 'commercial residential',
+            title: 'Small House Area',
+            city: 'NEW YORK',
+        },
+        {
+            id: '10',
+            img: 'images/team/14.png',
+            tag: 'highrise residential',
+            title: 'Modern Architecture',
+            city: 'NEW YORK',
+        },
+        {
+            id: '11',
+            img: 'images/team/16.png',
+            tag: 'business highrise',
+            title: 'Large Swiming Pool',
+            city: 'NEW YORK',
+        },
+        {
+            id: '12',
+            img: 'images/team/15.png',
+            tag: 'residential',
+            title: 'Modern Pent House',
+            city: 'NEW YORK',
+        },
+    ];
+
+    const [filterKey, setFilterKey] = useState('*')
+
+    useEffect(() => {
+        Isotope.current = new Isotope('.grid', {
+            itemSelector: '.element-item',
+            percentPosition: true,
+            // layoutMode: 'packery',
+            // masonry: {
+            //     columnWidth: '.grid-sizer'
+            // },
+            transformsEnabled: true,
+            transitionDuration: "700ms",
+            resize: true,
+            fitWidth: true,
+            // columnWidth: '.grid-sizer',
+        })
+
+        // cleanup
+        return () => Isotope.current.destroy()
+    }, []);
+
+    // handling filter key change
+    useEffect(() => {
+        filterKey === '*'
+            ? Isotope.current.arrange({ filter: `*` })
+            : Isotope.current.arrange({ filter: `.${filterKey}` })
+    }, [filterKey]);
+
+    const handleFilterKeyChange = key => () => setFilterKey(key);
+
     return (
         <div className="project_iso project_iso_bg">
             <div className="container">
@@ -16,7 +134,7 @@ const ProjectHome02 = () => {
                 </div>
                 <div className="row">
                     <div className="col">
-                        <Tabs>
+                        {/* <Tabs>
                             <TabList className="button-group filters-button-group">
                                 <Tab className="button is-checked" data-filter="*">All</Tab>
                                 <Tab className="button" data-filter=".commercial">Commercial</Tab>
@@ -330,8 +448,24 @@ const ProjectHome02 = () => {
 
 
                             </div>
-                        </Tabs>
+                        </Tabs> */}
+                        <div className="button-group filters-button-group button_color">
+                            <button className="button" onClick={handleFilterKeyChange('*')}>All <sup className="filter-count"></sup></button>
+                            <button className="button" onClick={handleFilterKeyChange('commercial')}>Commercial <sup className="filter-count"></sup></button>
+                            <button className="button" onClick={handleFilterKeyChange('highrise')}>Highrise <sup className="filter-count"></sup></button>
+                            <button className="button" onClick={handleFilterKeyChange('residential')}>Residential <sup className="filter-count"></sup></button>
+                            <button className="button" onClick={handleFilterKeyChange('business')}>Business <sup className="filter-count"></sup></button>
 
+                            <div className="grid grid-4 gutter-15">
+                                {
+                                    ProjectHomeData02.map(data => <ProjectCard01
+                                        key={data.id}
+                                        data={data}
+                                    />)
+                                }
+
+                            </div>
+                        </div>
 
                     </div>
                     <div className="col-12 box_bottom">
